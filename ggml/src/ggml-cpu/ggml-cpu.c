518faffe72dd31a85626fa7562f10f6f39e819cf
Aaron Teo <aaron.teo1@ibm.com>
2025-01-01 14:39:20 +0800

    ggml: add temporary patch for GGML_F32_ARR and GGML_F16_ARR
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -1308,6 +1308,9 @@ static inline void __lzs_f16cx4_store(ggml_fp16_t * x, __vector float y) {
 #define GGML_F16_VEC_MUL            GGML_F32x4_MUL
 #define GGML_F16_VEC_REDUCE         GGML_F32x4_REDUCE
 
+#define GGML_F32_ARR (GGML_F32_STEP/GGML_F32_EPR)
+#define GGML_F16_ARR (GGML_F16_STEP/GGML_F16_EPR)
+
 #endif
 
 // GGML_F32_ARR / GGML_F16_ARR
