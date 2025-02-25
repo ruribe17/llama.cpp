3fe208a6cfef266d378a6252d3d402ec82168a97
ochafik <ochafik@google.com>
2025-02-25 04:52:46 +0000

    support add_generation_prompt query parameter (useful for /apply_template)

--- examples/server/utils.hpp
+++ examples/server/utils.hpp
@@ -600,6 +600,7 @@ static json oaicompat_completion_params_parse(
     inputs.use_jinja             = use_jinja;
     inputs.parallel_tool_calls   = json_value(body, "parallel_tool_calls", false);
     inputs.extract_reasoning     = reasoning_format != COMMON_REASONING_FORMAT_NONE;
+    inputs.add_generation_prompt = json_value(body, "add_generation_prompt", true);
     if (!inputs.tools.empty() && inputs.tool_choice != COMMON_CHAT_TOOL_CHOICE_NONE && body.contains("grammar")) {
         throw std::runtime_error("Cannot use custom grammar constraints with tools.");
     }
