e9a1be0ab8978f8a8750319ffd557f063a3c2892
Alan Gray <alangray3@gmail.com>
2025-03-05 23:23:22 -0800

    CUDA: Simplify and improve CUDA graphs through use of indirect copy pointers
    Previously there was complexity in the CUDA graphs implementation due
    frequently changing parameters to copy kernels associated with K and V
    cache pointers. This patch simplifies by using indirection to avoid
    such parameters frequently changing, avoiding the need for frequent
    graph updates.
    
    Fixes #12152

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -711,7 +711,7 @@ struct ggml_cuda_graph {
     bool disable_due_to_failed_graph_capture = false;
     int number_consecutive_updates = 0;
     std::vector<ggml_graph_node_properties> ggml_graph_properties;
-    std::vector<char **> updated_kernel_arg;
+    std::vector<char *> cpy_dest_ptrs;
 #endif
 };
 
