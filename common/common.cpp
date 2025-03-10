5636696808d3a8eac66d3ab04118fd1bc06617d2
juk <jukofyork@yahoo.com>
2025-03-10 15:59:36 +0000

    Fixed typo in mla_attn name

--- common/common.cpp
+++ common/common.cpp
@@ -1132,7 +1132,7 @@ struct llama_context_params common_context_params_to_llama(const common_params &
     cparams.cb_eval_user_data = params.cb_eval_user_data;
     cparams.offload_kqv       = !params.no_kv_offload;
     cparams.flash_attn        = params.flash_attn;
-    cparams.mla_attn_attn     = params.mla_attn_attn;
+    cparams.mla_attn          = params.mla_attn;
     cparams.no_perf           = params.no_perf;
 
     if (params.reranking) {
