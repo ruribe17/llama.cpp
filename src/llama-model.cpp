69d61ee4f7cf7c5d92e4b28ba27269100f1fecc1
Si1w <stevenwu3721@163.com>
2025-03-18 19:38:38 +0000

    [Bug] Fix the bug in create attn kv

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -11475,7 +11475,7 @@ struct llm_build_plm : public llm_graph_context {
         // inp_pos - contains the positions
         ggml_tensor * inp_pos = build_inp_pos();
 
-        auto * inp_attn = build_attn_inp_kv_unified(true, false);
+        auto * inp_attn = build_attn_inp_kv_unified();
 
         for (int il = 0; il < n_layer; ++il) {
             ggml_tensor * inpSA = inpL;
